bf9a606596332df08b4be90e2f8f226a6e6db9e0
R <Rei@linyixuande-macbook-pro.local>
2016-10-03 19:32:41 +0800

    Initial Commit

---
+++ Demo161003/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo161003
+//
+//  Created by R on 2016/10/3.
+//  Copyright © 2016年 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
